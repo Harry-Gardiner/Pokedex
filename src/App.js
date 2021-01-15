de6b1219b3c489547c0d3b3fe31d3b769947645b
Harry Gardiner <gardiner_harry@yahoo.com>
2021-01-15 12:11:54 +0000

    GET list of pokemon call established

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
 
 function App() {
+  // List of pokemon - initially empty arr
+  const [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then(response => {
+      setPokemonData(response.data.results);
+    })
+  }, [])
+
+  // display list of pokemon
+  console.log(pokemonData);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Pokemon!</h1>
+
+    </>
   );
 }
 
