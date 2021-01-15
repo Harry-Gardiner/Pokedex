268f86c45133f094756de33cd21838fe781905af
Harry Gardiner <gardiner_harry@yahoo.com>
2021-01-15 13:55:52 +0000

    GET single pokemon from API established

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { useState, useEffect } from 'react';
-
 import axios from 'axios';
 
 function App() {
   // List of pokemon - initially empty arr
   const [pokemonData, setPokemonData] = useState([]);
+  const [pokemon, setPokemon] = useState("20"); // can be either number or name(lowercase)
 
-  useEffect(() => {
+  const getPokemon = () => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).then(response => {
+      console.log(response.data);
+    });
+  };
+
+  const getPokemonList = () => {
     axios.get("https://pokeapi.co/api/v2/pokemon").then(response => {
       setPokemonData(response.data.results);
-    })
-  }, [])
+    });
+  };
 
-  // display list of pokemon
-  console.log(pokemonData);
+  // Runs once on render
+  useEffect(() => {
+    //Get list of pokemon
+    getPokemonList()
+
+    // Get single pokemon
+    getPokemon();
+  }, []);
 
 
   return (
