339c0b0894d1e7e5714d87283bd7508af6feae19
Harry Gardiner <gardiner_harry@yahoo.com>
2021-01-15 15:21:01 +0000

    Initial search from completed

---
+++ src/Components/Search/SearchPokemon.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const SearchPokemon = ({ getPokemon }) => {
+    const [value, setValue] = useState("") // set initial value of search input
+
+    const handleChange = (e) => {
+        setValue(e.currentTarget.value.toLowerCase());
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        getPokemon(value);
+        setValue("");
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label> Enter Pokemon Name or ID </label>
+            <input
+                type="text"
+                onChange={handleChange}
+                value={value}
+                placeholder="Pikachu / 25"
+            />
+        </form>
+    )
+};
+
+export default SearchPokemon;
