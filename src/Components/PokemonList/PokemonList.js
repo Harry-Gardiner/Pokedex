b3ae5feae44332e84a70ed85f224c46f4f408015
Harry Gardiner <gardiner_harry@yahoo.com>
2021-01-18 13:15:08 +0000

    PokemonList next and previous buttons added. Logic increments pokemon displayed. GetPokemon request limit set to 151, so only 1st gen pokemon are returned

--- src/Components/PokemonList/PokemonList.js
+++ src/Components/PokemonList/PokemonList.js
@@ -1,11 +1,43 @@
+import React, { useState } from 'react';
 import { capitalize } from '../../functions/functions';
 
 const PokemonList = ({ data }) => {
+    const [listCount, setListCount] = useState(0);
+
+    const PokemonList = [];
+
+    // Add +5 
+    for (let i = listCount; i < listCount + (listCount === 140 ? 11 : 20); i += 1) {
+        PokemonList.push(data[i]);
+    };
+
+    const gotoNextPage = () => {
+        console.log("next");
+        setListCount(listCount + 20)
+    };
+
+    const gotoPrevPage = () => {
+        console.log("prev");
+        setListCount(listCount - 20)
+    };
+
     return (
         <>
             <h2>Pokemon List</h2>
+            <button
+                onClick={gotoPrevPage}
+                disabled={listCount === 0 ? true : false}
+            >
+                Previous
+            </button>
+            <button
+                onClick={gotoNextPage}
+                disabled={listCount === 140 ? true : false}
+            >
+                Next
+            </button>
             <div className="listContainer">
-                {data.map((pokemon, index) => (
+                {PokemonList.map((pokemon, index) => (
                     <div
                         key={pokemon.name}
                         className="list-card"
